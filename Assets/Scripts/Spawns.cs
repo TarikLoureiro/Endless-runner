05fde313f8b455663db3910facb0851659d22dbe
TarikLoureiro <151021439+TarikLoureiro@users.noreply.github.com>
2023-12-04 11:25:15 +0000

    Spawn de estradas

--- Assets/Scripts/Spawns.cs
+++ Assets/Scripts/Spawns.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Spawns : MonoBehaviour
 {
-    public GameObject Estrada;
+    public GameObject estrada;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(estrada, new Vector3(-0.0017561f, -0.0037286f, 5.585f), Quaternion.identity);
     }
 
     // Update is called once per frame
