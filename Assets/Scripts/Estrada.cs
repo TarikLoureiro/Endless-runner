0ac939b883df017d5800f2fbd07b5a952bc0e7e5
TarikLoureiro <151021439+TarikLoureiro@users.noreply.github.com>
2023-12-05 11:57:40 +0000

    movimento estrada(not complete)

--- Assets/Scripts/Estrada.cs
+++ Assets/Scripts/Estrada.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Estrada : MonoBehaviour
 {
+    public GameObject estrada;
+
+
+    private float velocidadecenario = 0.05f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class Estrada : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        estrada.transform.Translate(0, 0, velocidadecenario * -1);
     }
 }
